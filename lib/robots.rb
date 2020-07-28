988b5849074745cc7fbe77af1a65f37dabc3cb51
temadoomsday <temadoomsday@gmail.com>
2020-07-28 16:08:01 +0300

    Added custom robots txt receiving

--- lib/robots.rb
+++ lib/robots.rb
@@ -1,23 +1,18 @@
-require "open-uri"
-require "uri"
-require "rubygems"
-require "timeout"
+# frozen_string_literal: true
+
+require 'open-uri'
+require 'uri'
+require 'rubygems'
+require 'timeout'
 
 class Robots
-  
   DEFAULT_TIMEOUT = 3
-  
-  class ParsedRobots
-    
-    def initialize(uri, user_agent)
-      @last_accessed = Time.at(1)
-      
-      io = Robots.get_robots_txt(uri, user_agent)
-      
-      if !io || io.content_type != "text/plain" || io.status != ["200", "OK"]
-        io = StringIO.new("User-agent: *\nAllow: /\n")
-      end
+  DEFAULT_ROBOTS = "User-agent: *\nAllow: /\n"
 
+  class ParsedRobots
+    def initialize(robots_content)
+      io = robots_content
+      @last_accessed = Time.at(1)
       @other = {}
       @disallows = {}
       @allows = {}
@@ -25,113 +20,122 @@ class Robots
       agent = /.*/
       io.each do |line|
         next if line =~ /^\s*(#.*|$)/
-        arr = line.split(":")
+
+        arr = line.split(':')
         key = arr.shift
-        value = arr.join(":").strip
+        value = arr.join(':').strip
         value.strip!
         case key
-        when "User-agent"
+        when 'User-agent'
           agent = to_regex(value)
-        when "Allow"
+        when 'Allow'
           @allows[agent] ||= []
           @allows[agent] << to_regex(value)
-        when "Disallow"
+        when 'Disallow'
           @disallows[agent] ||= []
           @disallows[agent] << to_regex(value)
-        when "Crawl-delay"
+        when 'Crawl-delay'
           @delays[agent] = value.to_i
         else
           @other[key] ||= []
           @other[key] << value
         end
       end
-      
+
       @parsed = true
     end
-    
+
     def allowed?(uri, user_agent)
       return true unless @parsed
+
       allowed = true
       path = uri.request_uri
-      
+
       @disallows.each do |key, value|
-        if user_agent =~ key
-          value.each do |rule|
-            if path =~ rule
-              allowed = false
-            end
-          end
+        next unless user_agent =~ key
+
+        value.each do |rule|
+          allowed = false if path =~ rule
         end
       end
-      
+
       @allows.each do |key, value|
-        unless allowed      
-          if user_agent =~ key
-            value.each do |rule|
-              if path =~ rule
-                allowed = true
-              end
-            end
-          end
+        next if allowed
+        next unless user_agent =~ key
+
+        value.each do |rule|
+          allowed = true if path =~ rule
         end
       end
-      
+
       if allowed && @delays[user_agent]
         sleep @delays[user_agent] - (Time.now - @last_accessed)
         @last_accessed = Time.now
       end
-      
-      return allowed
+
+      allowed
     end
-    
+
     def other_values
       @other
     end
-    
-  protected
-    
+
+    protected
+
     def to_regex(pattern)
       return /should-not-match-anything-123456789/ if pattern.strip.empty?
+
       pattern = Regexp.escape(pattern)
-      pattern.gsub!(Regexp.escape("*"), ".*")
+      pattern.gsub!(Regexp.escape('*'), '.*')
       Regexp.compile("^#{pattern}")
     end
   end
-  
+
   def self.get_robots_txt(uri, user_agent)
-    begin
-      Timeout::timeout(Robots.timeout) do
-        io = URI.join(uri.to_s, "/robots.txt").open("User-Agent" => user_agent) rescue nil
-      end 
-    rescue Timeout::Error
-      STDERR.puts "robots.txt request timed out"
+    Timeout.timeout(Robots.timeout) do
+      begin
+        URI.join(uri.to_s, '/robots.txt').open('User-Agent' => user_agent)
+      rescue StandardError
+        nil
+      end
     end
+  rescue Timeout::Error
+    warn 'robots.txt request timed out'
   end
-  
-  def self.timeout=(t)
-    @timeout = t
+
+  class << self
+    attr_writer :timeout
   end
-  
+
   def self.timeout
     @timeout || DEFAULT_TIMEOUT
   end
-  
-  def initialize(user_agent)
+
+  def initialize(user_agent, custom_robots_txt = nil)
     @user_agent = user_agent
+    @custom_robots_txt = custom_robots_txt
     @parsed = {}
   end
-  
+
+  def robots_content(uri)
+    return @custom_robots_txt if @custom_robots_txt
+
+    io = Robots.get_robots_txt(uri, @user_agent)
+    io = StringIO.new(Robots::DEFAULT_ROBOTS) if !io || io.content_type != 'text/plain' || io.status != %w[200 OK]
+    io
+  end
+
   def allowed?(uri)
     uri = URI.parse(uri.to_s) unless uri.is_a?(URI)
     host = uri.host
-    @parsed[host] ||= ParsedRobots.new(uri, @user_agent)
+    @parsed[host] ||= ParsedRobots.new(robots_content(uri))
     @parsed[host].allowed?(uri, @user_agent)
   end
-  
+
   def other_values(uri)
     uri = URI.parse(uri.to_s) unless uri.is_a?(URI)
     host = uri.host
-    @parsed[host] ||= ParsedRobots.new(uri, @user_agent)
+    @parsed[host] ||= ParsedRobots.new(robots_content(uri))
     @parsed[host].other_values
   end
 end
